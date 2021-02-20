21783c99420bff82bd6f07046076c0adf01ff36f
Kunju Perath <kperath@gmail.com>
2021-02-20 13:41:55 -0500

    changed way file was uploaded because there was issues with react-form-hooks

---
+++ ui/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/api")
+def hello():
+    return "hello"
+
+
+@app.route("/api/upload", methods=["POST"])
+def upload():
+    if request.method == "POST":
+        print("processing....")
+        # TODO: insert pytorch function here!
+        imageFile = request.files["file"]
+        imageFile.save(os.path.join("public/uploads", imageFile.filename))
+        return "Image Upload Successful!"
